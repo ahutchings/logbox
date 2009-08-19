1718ec06a335ee1de0286760330e0ec21432591a
Andrew Hutchings <andrew.hutchings@gmail.com>
2009-08-19 05:24:11 +0000

    Convert Messages model to Kohana ORM model.

--- application/models/message.php
+++ application/models/message.php
@@ -1,75 +1,7 @@
 <?php
 
-class Messages
+class Message_Model extends ORM
 {
-    public static function get_count()
-    {
-        $q = 'SELECT COUNT(1) FROM message';
-
-        $count = DB::connect()->query($q)->fetchColumn();
-
-        return $count;
-    }
-
-    /**
-     * get a message or messages
-     *
-     * @paramarray array Query parameters
-     *
-     * @return array An array of Message objects, or a single Message object, depending on request
-     */
-    public static function get($paramarray = array())
-    {
-        // defaults
-        $where  = array();
-        $params = array();
-        $limit  = is_numeric(Options::get('pagination')) ? Options::get('pagination') : 20;
-
-        // extract overrides
-        $allowed    = array('criteria', 'limit', 'offset', 'page');
-        $paramarray = array_intersect_key($paramarray, array_fill_keys($allowed, true));
-        extract($paramarray);
-
-        if (isset($page) && is_numeric($page) ) {
-            $offset = (intval($page) - 1) * intval($limit);
-        }
-
-        if (isset($criteria)) {
-            $where[] = "(sender LIKE CONCAT('%',?,'%') OR content LIKE CONCAT('%',?,'%'))";
-            $params[] = $criteria;
-            $params[] = $criteria;
-        }
-
-        $q = "SELECT * FROM message ";
-
-        if (count($where)) {
-            $q .= ' WHERE (' . implode(' AND ', $where) . ')';
-        }
-
-        $q .= " ORDER BY sent_at DESC";
-        $q .= " LIMIT $limit";
-
-        if (isset($offset)) {
-            $q .= " OFFSET $offset";
-        }
-
-        try {
-            $sth = DB::connect()->prepare($q);
-
-            $sth->setFetchMode(PDO::FETCH_CLASS, 'Message', array());
-
-            $sth->execute($params);
-
-            $messages = $sth->fetchAll();
-        } catch (PDOException $e) {
-            trigger_error($e->getMessage());
-
-            return false;
-        }
-
-        return $messages;
-    }
-
     /**
      * initial import action
      *
