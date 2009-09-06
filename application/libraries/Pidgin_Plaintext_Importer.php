3a0361293204049d04a79ae48783c0d455e39a87
Andrew Hutchings <andrew.hutchings@gmail.com>
2009-09-06 01:15:03 +0000

    Use consistent terminology; refactor path normalization code into a method.

--- application/libraries/Pidgin_Plaintext_Importer.php
+++ application/libraries/Pidgin_Plaintext_Importer.php
@@ -11,44 +11,40 @@ class Pidgin_Plaintext_Importer extends Repository_Importer
     {
         $message_regex = '/^\((?P<sentat>.*?)\) (?P<sender>.*?): (?P<content>.*)/';
         $status_regex  = '/\\((?P<sentat>.*)\\) (?P<sender>.*)\\ (?P<content>.*)/';
-        $file_regex    = '%/(?P<protocol>.*)/(?P<account>.*)/(?P<recipient>.*)/'
+        $path_regex    = '%/(?P<protocol>.*)/(?P<account>.*)/(?P<buddy>.*)/'
             .'(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2}).(?P<hour>\d{2})(?P<minute>\d{2})(?P<second>\d{2})%';
 
-        foreach (dir::list_files($this->directory) as $path) {
+        foreach (dir::list_files($this->directory) as $file) {
 
-            // standardize path for the regex
-            $file = substr($path, strlen(implode(DIRECTORY_SEPARATOR, explode(DIRECTORY_SEPARATOR, $path, -4))));
-            $file = str_replace('\\', '/', $file);
-
-            preg_match($file_regex, $file, $file_match);
+            preg_match($path_regex, self::normalize_path($file), $path_match);
 
             // create entry for protocol
-            $protocol = ORM::factory('protocol')->where('name', $file_match['protocol'])->find();
+            $protocol = ORM::factory('protocol')->where('name', $path_match['protocol'])->find();
             if (!$protocol->loaded) {
-                $protocol->name = $file_match['protocol'];
+                $protocol->name = $path_match['protocol'];
                 $protocol->save();
             }
 
             // create entry for account
-            $account = ORM::factory('account')->where('name', $file_match['account'])->find();
+            $account = ORM::factory('account')->where('name', $path_match['account'])->find();
             if (!$account->loaded) {
-                $account->name = $file_match['account'];
+                $account->name = $path_match['account'];
                 $account->protocol_id = $protocol->id;
                 $account->save();
             }
 
             // create entry for buddy
-            $buddy = ORM::factory('buddy')->where('name', $file_match['recipient'])->find();
+            $buddy = ORM::factory('buddy')->where('name', $path_match['buddy'])->find();
             if (!$buddy->loaded) {
-                $buddy->name = $file_match['recipient'];
+                $buddy->name = $path_match['buddy'];
                 $buddy->account_id = $account->id;
                 $buddy->save();
             }
 
             // create entry for conversation
             $conv_started_at = date('Y-m-d H:i:s', strtotime(
-                $file_match['year'].'-'.$file_match['month'].'-'.$file_match['day'].' '
-                .$file_match['hour'].':'.$file_match['minute'].':'.$file_match['second']
+                $path_match['year'].'-'.$path_match['month'].'-'.$path_match['day'].' '
+                .$path_match['hour'].':'.$path_match['minute'].':'.$path_match['second']
             ));
 
             $conversation = ORM::factory('conversation')
@@ -66,7 +62,7 @@ class Pidgin_Plaintext_Importer extends Repository_Importer
                 $conversation->save();
             }
 
-            $lines = file($path);
+            $lines = file($file);
 
             for ($i = 1, $n = count($lines); $i < $n; $i++) {
 
@@ -84,7 +80,7 @@ class Pidgin_Plaintext_Importer extends Repository_Importer
                     $message_match['sender'] = str_replace(' <AUTO-REPLY>', '', $message_match['sender']);
 
                     // format time
-                    $time = $file_match['year'] .'-'. $file_match['month'] .'-'. $file_match['day']
+                    $time = $path_match['year'] .'-'. $path_match['month'] .'-'. $path_match['day']
                         .' '. $message_match['sentat'];
                     $time = date('Y-m-d H:i:s', strtotime($time));
 
@@ -93,7 +89,7 @@ class Pidgin_Plaintext_Importer extends Repository_Importer
                     $message->conversation_id = $conversation->id;
                     $message->sent_at   = $time;
                     $message->sender    = $message_match['sender'];
-                    $message->recipient = $file_match['recipient'];
+                    $message->recipient = $path_match['buddy'];
                     $message->content   = $message_match['content'];
                     $message->save();
 
@@ -104,5 +100,23 @@ class Pidgin_Plaintext_Importer extends Repository_Importer
                 }
             }
         }
+    }
+
+    /**
+     * Normalizes a path.
+     *
+     * @param string $path The file path
+     *
+     * @return string
+     */
+    private static function normalize_path($path)
+    {
+        // remove extra directories
+        $path = substr($path, strlen(implode(DIRECTORY_SEPARATOR, explode(DIRECTORY_SEPARATOR, $path, -4))));
+
+        // apply a consistent directory separator
+        $path = str_replace('\\', '/', $path);
+
+        return $path;
     }
 }
